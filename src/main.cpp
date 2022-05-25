46e0738905eda5f4e859c21e262f314754fca9ed
jlxip <jlxip@protonmail.com>
2022-05-25 23:12:00 +0200

    Test RPC client

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <syscalls>
 
 extern "C" void _start() {
-	if(std::resolve("term") == 0x2)
-		std::exit(0x42);
-	else
-		std::exit(0x0);
+	PID term = std::resolve("term");
+	if(term != 0x2)
+		std::exit(0x1);
+
+	bochsbrk();
+	std::rpc(term, 0, 'X');
+	std::rpc(term, 1, 13, 6);
+
+	while(true);
 }
