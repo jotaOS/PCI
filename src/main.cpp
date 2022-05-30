40e6e8fab312b7e799a8ad75219af8e4788947ac
jlxip <jlxip@protonmail.com>
2022-05-30 23:21:28 +0200

    Halting and that's it

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,4 @@
+#include <cstdio>
 #include <syscalls>
 
-extern "C" void _start() {
-	PID term = std::resolve("term");
-	if(term != 0x2)
-		std::exit(0x1);
-
-	bochsbrk();
-	std::rpc(term, 0, 'X');
-	std::rpc(term, 1, 13, 6);
-
-	while(true);
-}
+extern "C" void _start() { std::halt(); }
